51e830b4f0364ae8a170514762a5ef3fe7ed28ee
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-09-09 20:45:44 +0600

    static members work done

--- static-members.php
+++ static-members.php
@@ -12,7 +12,7 @@ Static properties are declared with the static keyword: -->
     personal::show(); */
 
     // example - 2 how to use static parent class properties to the child class
-    class personal{
+    /* class personal{
         public static $name = "Sujon Ahmed";
     }
     class account extends personal{
@@ -21,6 +21,20 @@ Static properties are declared with the static keyword: -->
         }
     }
     $obj = new account();
-    $obj->show();
+    $obj->show(); */
+
+    // example- 3 static method with inheritance
+    class xyz{
+        protected static $name = "Parent";
+    }
+    class abc extends xyz{
+        public static $name = "Child";
+        public function dis(){
+            // return self::$name;
+            return parent::$name;
+        }
+    }
+    $obj = new abc();
+    echo $obj->dis();
 
 ?>
