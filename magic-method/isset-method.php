56e9a3e33b69390ecac6c1f980aade807a078c5f
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-09-14 16:40:20 +0600

    easy way of isset magic method

--- magic-method/isset-method.php
+++ magic-method/isset-method.php
@@ -1,24 +1,46 @@
 <?php
+// example -1
+class abc{
+    public $name;
+    private $roll;
+    private $age;
 
-class student{
-    public $course;
-    private $first_name;
-    private $last_name;
-    private $details = ['name'=>'test name','age'=>'20'];
-
-    public function setName($fname, $lname){
-        $this->first_name = $fname;
-        $this->last_name = $lname;
+    public function show($roll,$age){
+        $this->age = $age;
+        $this->roll = $roll;
     }
-    public function __isset($details)
+    // use for check private property isset or not
+    public function __isset($property)
     {
-        echo isset($this->details[$details]);
-    }
+        echo isset($this->$property);
+    } 
 }
-$obj = new student();
+$obj = new abc();
+// $obj->name = "Sujon";
+// echo isset($obj->name); //result is 1 because the name property is fill by value
+$obj->show(923364,21);
+echo isset($obj->roll);
+
+// example -2
+// class student{
+//     public $course;
+//     private $first_name;
+//     private $last_name;
+//     private $details = ['name'=>'test name','age'=>'20'];
+
+//     public function setName($fname, $lname){
+//         $this->first_name = $fname;
+//         $this->last_name = $lname;
+//     }
+//     public function __isset($details)
+//     {
+//         echo isset($this->details[$details]);
+//     }
+// }
+// $obj = new student();
 // $obj->course = "PHP";
 // echo isset($obj->course);
-echo isset($obj->name);
+// echo isset($obj->name);
 // echo empty($obj->city);
 
 ?>
