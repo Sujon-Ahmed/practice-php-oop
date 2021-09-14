3e73696f2d1f5a24ff1c9be127f51b1441c1cc89
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-09-14 17:00:29 +0600

    learn about unset-method

--- magic-method/unset-method.php
+++ magic-method/unset-method.php
@@ -4,9 +4,22 @@ class abc{
     public $name = "Sujon Ahmed";
     private $roll;
     private $age;
+
+    public function show($roll,$age){
+        $this->roll = $roll;
+        $this->age = $age;
+    }
+    public function __unset($property){
+        unset($this->$property);
+    }
 }
 $obj = new abc();
-unset ($obj->name);
-echo $obj->name; // for public property name we can easily unset 
+// unset ($obj->name);
+// echo $obj->name; // for public property name we can easily unset 
+$obj->show(923364,21);
+echo "<pre>";
+unset($obj->roll);
+print_r($obj);
+echo "</pre>";
 
 ?>
