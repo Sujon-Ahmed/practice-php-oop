92e3f3d37744745ee6ed31817544000bfddbd840
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-09-14 10:02:51 +0600

    call-method access done

--- magic-method/call-method.php
+++ magic-method/call-method.php
@@ -3,7 +3,7 @@
     2.__call($method,$arg) take two aru for showing this property name and value.
  -->
  <?php
-
+// example -1
 /* class student{
     private $first_name;
     private $last_name;
@@ -23,5 +23,30 @@
 $obj = new student();
 $obj->setName("Sujon", "Ahmed");
 // $obj->personal(); */
+
+// example -2
+
+class student{
+    private $first_name;
+    private $last_name;
+
+    private function setName($fname, $lname){
+        $this->first_name = $fname;
+        $this->last_name = $lname;
+    }
+    public function __call($method, $arg)
+    {
+        if(method_exists($this, $method)){
+            call_user_func_array([$this, $method], $arg);
+        }else{
+            echo "Method does not exist : $method";
+        }
+    }
+}
+$obj = new student();
+$obj->setName("Sujon", "Ahmed");
+echo "<pre>";
+print_r($obj);
+echo "</pre>";
 
 ?>
