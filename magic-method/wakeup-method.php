8ea7a6e59d0b3ad9c5b4d658f2138660a9e702ea
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-09-16 17:25:00 +0600

    the real example of wakeup method in database

--- magic-method/wakeup-method.php
+++ magic-method/wakeup-method.php
@@ -4,19 +4,26 @@ class student{
     public $course = "PHP";
     private $first_name;
     private $last_name;
+    private $conn;
 
     public function setName($fname,$lname){
         $this->first_name = $fname;
         $this->last_name = $lname;
     }
+    public function __construct()
+    {
+        // $this->conn = mysqli_connect();
+    }
 
     public function __sleep()
     {
+        // mysqli_close($this->conn);
         return array("first_name","last_name");
     }
 
     public function __wakeup()
     {
+        // $this->conn = mysqli_connect();
         echo 'This is wakeup method';
     }
 }
