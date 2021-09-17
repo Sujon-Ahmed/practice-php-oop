c88cd57f47947f970b0c148f03ee3bb611565ab9
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-09-17 21:44:47 +0600

    constants function

--- magic-method/magic-constants/magic-constants.php
+++ magic-method/magic-constants/magic-constants.php
@@ -7,7 +7,14 @@
 // echo "The full path of this file : " .__FILE__;
 
 // magic constants __DIR__
-echo "The full path of this directory : " .__DIR__;
+// echo "The full path of this directory : " .__DIR__;
+
+// magic constants __FUNCTION__
+// echo "The FUNCTION Name : " .__FUNCTION__; // RETURN Nothing
+function myFunction(){
+    echo "The FUNCTION Name : " .__FUNCTION__; // NOW RETURN FUNCTION NAME
+}
+myFunction();
 
 
 ?>
