3fc6fafc14ab69af8bd9e09cf1734652d1f4bd49
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-09-17 21:37:45 +0600

    file constants

--- magic-method/magic-constants/magic-constants.php
+++ magic-method/magic-constants/magic-constants.php
@@ -1,5 +1,10 @@
 <?php
 // magic constants __LINE__
-echo "Line Number : " .__LINE__;
+// echo "Line Number : " .__LINE__."<br>";
+// echo "Line Number : " .__LINE__;
+
+// magic constants __FILE__
+// echo "The full path of this file : " .__FILE__;
+
 
 ?>
